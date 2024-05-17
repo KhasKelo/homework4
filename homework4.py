66c004b49ad46dca557b836ec60cb553c4592b9e
KhasKelo <sir.yangulbaev@gmail.com>
2024-05-17 15:20:54 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+immutable_var = ('Khasan' , 31 , True , [9,5])
+print(immutable_var)
+
+#immutable_var[0] = 66
+#print(immutable_var)
+# Нельзя изменить значения в кортеже, так как он не изиеняем
+
+mutable_list = ["studet", 2024 , False]
+mutable_list[1] = 2023
+print(mutable_list)
